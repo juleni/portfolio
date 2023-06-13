62d5f62078ee110685b1b58c7ed43a6dab272650
juleni <69912179+juleni@users.noreply.github.com>
2023-06-13 12:03:32 +0200

    Added Work component

---
+++ src/components/Work.jsx
@@ -0,0 +1,59 @@
+import WorkItem from "./WorkItem";
+
+const data = [
+  {
+    year: 2021,
+    title: "Software Engineer",
+    duration: "till now",
+    details:
+      "Full stack programmer / Analyst. Tech stack: Javascript, Typescript, Node.js, React, Java, #C, SQL, NoSQL",
+  },
+  {
+    year: 2016,
+    title: "e-Shop administrator, manager",
+    duration: "5 years",
+    details:
+      "Administration of e-shops, processing of orders (supplier’s and wholesale customer‘s), handling of complaints, consulting, communication with customers, registration of raw materials for the production of beeswax foundations, organization in the unloading and storage of goods from suppliers, translations and creation of manuals for beekeeping equipment, providing other services necessary for the operation of the company.",
+  },
+  {
+    year: 2006,
+    title: "Real Estate Agent",
+    duration: "10 years",
+    details:
+      "Mediation and sale of real estates, primary focused on foreign clients, arranging of formalities and other services, arranging of reconstruction of real estates.",
+  },
+  {
+    year: 2003,
+    title: "Full stack programmer",
+    duration: "3 years",
+    details:
+      "Open source project. Tech stack: Java/J2EE, Javascript, HTML, CSS, RDBs",
+  },
+  {
+    year: 2002,
+    title: "SQL programmer",
+    duration: "1 year",
+    details: "Tech stack: PostgreSQL",
+  },
+  {
+    year: 2000,
+    title: "Fullstack programmer",
+    duration: "2 years",
+    details: "Tech stack: Java/JSP, Javascript, mysql, HTML, CSS",
+  },
+];
+
+const Work = () => {
+  return (
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-10">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e] pb-5">
+        Work
+      </h1>
+      {data.map((item, idx) => (
+        <WorkItem key={idx} item={item} />
+      ))}
+    </div>
+  );
+};
+
+export default Work;
