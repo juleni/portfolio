991f5b7e93e4faed6a333a84884fbd36109f153a
juleni <69912179+juleni@users.noreply.github.com>
2023-06-13 15:57:43 +0200

    fixed href

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -84,7 +84,7 @@ const Sidenav = () => {
             <AiOutlineHome size={20} />
           </a>
           <a
-            href="work"
+            href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer 
               hover:scale-110 ease-in duration-300"
           >
