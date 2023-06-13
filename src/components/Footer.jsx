7b42d068023b4a9611a64afe072c66a79ae96e79
juleni <69912179+juleni@users.noreply.github.com>
2023-06-13 17:35:38 +0200

    Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="w-full text-center pb-5">
+      <span>
+        2023 Created by{" "}
+        <a
+          href="https://github.com/juleni"
+          target={"_blank"}
+          rel="noreferrer"
+          className="font-semibold hover:underline "
+        >
+          JULENI
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
